63805f21839e96c928f43218b08d03370e174bbb
221RDB047 <lukas.pahomovs@edu.rtu.lv>
2023-03-05 23:11:45 +0200

    221RDB047

--- tree_height.py
+++ tree_height.py
@@ -38,7 +38,7 @@ def main():
             print("File not found")
             return
     #print(array)
-    #print(compute_height(array, 0))
+    print(compute_height(array, 0))
      
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
@@ -46,7 +46,7 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    print(compute_height([4, -1, 4, 1, 1],1))
+    #print(compute_height([4, -1, 4, 1, 1],1))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
